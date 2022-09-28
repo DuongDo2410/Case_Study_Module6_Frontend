d597435e4417d5b10066825901e7ad7380441757
DuongDo2410 <66940364+DuongDo2410@users.noreply.github.com>
2022-09-28 15:05:29 +0700

    update master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/header/header";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      Link
+      <h1 className="text-xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Header;
