bc9429f6fecce7de43040ceae87545bfd5ef0863
ha.nguyen1003 <hanguyen18052004@gmail.com>
2022-09-29 16:18:33 +0700

    update profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import UpdateProfile from "./component/userProfile/UpdateProfile";
+import BasicList from "./component/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {/*<h1 className="text-3xl font-bold underline">Hello world!</h1>*/}
+      {/*  <UpdateProfile></UpdateProfile>*/}
+        <UpdateProfile/>
     </div>
+
   );
 }
 
