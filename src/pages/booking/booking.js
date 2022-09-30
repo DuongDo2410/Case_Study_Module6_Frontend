998dfea15baf0375237a896a94cfa5bae76f4bb3
DuongDo2410 <66940364+DuongDo2410@users.noreply.github.com>
2022-09-30 09:24:26 +0700

    update ui

--- src/pages/booking/booking.js
+++ src/pages/booking/booking.js
@@ -151,7 +151,7 @@ const Booking = () => {
                 <span className="title-font font-medium text-2xl text-gray-900">
                   $58.00
                 </span>
-                <button className="flex ml-auto text-white bg   -[#14f1d7] border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
+                <button className="flex ml-auto text-white bg-[#14f1d7] border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
                   Booking
                 </button>
                 <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
